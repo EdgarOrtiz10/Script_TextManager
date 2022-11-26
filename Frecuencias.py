452cd248c7fb988fe0a97343ea60ef3d953f47bc
EdgarOrtiz10 <2120eaoe@gmail.com>
2022-11-26 14:21:00 -0500

    Componente Frecuencias

--- Frecuencias.py
+++ Frecuencias.py
@@ -3,5 +3,26 @@ import nltk
 import string
 
 class Frecuencias: 
-    def __init__(self, data):
-        self.data = data
+    def frecuencias(dataIn):
+        global stopwords, data, frecuencia_palabras
+        data = dataIn 
+        nltk.download('punkt')
+        nltk.download('stopwords')
+        stopwords = nltk.corpus.stopwords.words('spanish') 
+        string.punctuation #!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+        
+        def preproceso(text):
+            formatear_texto = text.lower()
+            tokens = []
+            for token in nltk.word_tokenize(formatear_texto):
+                tokens.append(token)
+            #print(tokens)
+            #tokens = [word for word in tokens if word not in stopwords]
+            tokens = [word for word in tokens if word not in stopwords and word not in string.punctuation ]
+            formatear_texto = ' '.join(element for element in tokens)
+            return formatear_texto
+        
+        formatear_texto = preproceso(data)
+                
+        #Frecuencia de palabras
+        frecuencia_palabras = nltk.FreqDist(nltk.word_tokenize(formatear_texto))
